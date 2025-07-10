62cd34dc2794e38019b4586d46075fc077c926bb
Yan Velasco <yancarlosduartevelasco@gmail.com>
2025-07-10 13:10:14 -0300

    initialize Spring Boot project with Maven wrapper and essential configurations

---
+++ src/test/java/com/ecom/testeinter/TesteInterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.testeinter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteInterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
